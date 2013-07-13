16281f6c1c873fef9200b9186f472193fe353d6b
Zhipeng Hu <larryhujob@gmail.com>
2013-07-13 17:21:08 -0400

    leetCode Solution

--- Maximum Depth of Binary Tree.cpp
+++ Maximum Depth of Binary Tree.cpp
@@ -20,3 +20,42 @@ public:
         return max(left,right) + 1;
     }
 };
+
+
+class Solution {
+public:
+	int maxDepth(TreeNode *root){
+		// Start typing your C/C++ solution below
+		// DO NOT write int main() function
+		if(root == NULL)
+			return 0;
+		stack<TreeNode*> sta;
+		TreeNode* prev = NULL;
+		sta.push(root);
+		int maxHeight = 0;
+		while(!sta.empty())
+		{
+			TreeNode* current = sta.top();
+			if(prev == NULL || prev->left == current || prev->right == current)
+			{
+				if(current->left != NULL)
+					sta.push(current->left);
+				else if(current->right != NULL)
+					sta.push(current->right);
+			}
+			else if(current->left == prev)
+			{
+				if(current->right != NULL)
+					sta.push(current->right);
+			}
+			else
+			{
+				sta.pop();
+			}
+			prev = current;
+			if(sta.size() > maxHeight)
+				maxHeight = sta.size();
+		}
+		return maxHeight;
+	}
+};
