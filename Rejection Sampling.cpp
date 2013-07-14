68d9c3327c8bbe5720efb219965c7f7c93fbd7b9
Zhipeng Hu <larryhujob@gmail.com>
2013-07-14 00:57:45 -0400

    LeetCode Solution

--- Rejection Sampling.cpp
+++ Rejection Sampling.cpp
@@ -9,3 +9,21 @@ int rejectionSampling()
 			return num % 10 + 1;
 	}
 }
+
+
+int rejectionSamplingBetter()
+{
+	while(1)
+	{
+		int num = 7*(rand7()-1) + (rand7()-1);
+		if(num < 40)
+			return num % 10 + 1;
+		num = 7*(num-40) + (rand7()-1);
+		if(n < 60)
+			return num % 10 + 1;
+		num = 7*(num-60) + (rand7()-1);
+		if(n < 20)
+			return num % 10 + 1;
+	}
+
+}
