5296c14e81ce76085604cbca33284279b91fce59
Zhipeng Hu <larryhujob@gmail.com>
2013-07-22 02:53:24 -0400

    LeetCode Solution

--- Finding intersection of two sorted arrays.cpp
+++ Finding intersection of two sorted arrays.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> findIntersection(vector<int>& A, vector<int>& B)
@@ -26,3 +27,17 @@ vector<int> findIntersection(vector<int>& A, vector<int>& B)
 	}
 	return ret;
 }
+
+vector<int> findIntersection(vector<int>& A, vector<int>& B)
+{
+	vector<int> ret;
+	if(A.empty() || B.empty())
+		return ret;
+	int m = A.size();
+	int n = B.size();
+	for(int i = 0; i < m; i++)
+		if(binary_search(B.begin(),B.end(),A[i]))
+			ret.push_back(A[i]);
+	return ret;
+}
+
